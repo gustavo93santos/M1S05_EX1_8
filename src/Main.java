23334e8b14f89808928b2767e2602bf6eca053b9
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-02-10 14:28:48 -0300

    [M1S05] EX3 - Adição List e ordenação

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando Jogo!");
 
-        Jogador j = new Jogador("Gustavo", 30, 0,0);
-
+        List jogadores = new ArrayList<>();
+        jogadores.add(new Jogador("Leonardo", 30, 10,2));
+        jogadores.add(new Jogador("Andre", 30, 15,3));
+        jogadores.add(new Jogador("Gustavo", 30, 5,2));
         System.out.println("Carga finalizada");
-        System.out.println(j.toString());
-        j.adicionaTentativa();
-        j.adicionaPontos(5);
-        System.out.println(j.toString());
+
+        ordenaLista(jogadores);
+    }
+
+    public static void ordenaLista(List jogadores) {
+        jogadores.sort(Comparator.comparingLong(Jogador::getPontuacao).reversed());
+        jogadores.forEach(System.out::println);
     }
 }
 
