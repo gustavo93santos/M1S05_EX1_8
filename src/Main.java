2efa3d9108b1f691def4b91e975c9f7ee7317d5f
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-02-10 18:47:03 -0300

    [M1S05] EX4 - Adição Listagem Ranking

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,33 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando Jogo!");
 
-        List jogadores = new ArrayList<>();
+        List<Jogador> jogadores = new ArrayList<>();
         jogadores.add(new Jogador("Leonardo", 30, 10,2));
         jogadores.add(new Jogador("Andre", 30, 15,3));
-        jogadores.add(new Jogador("Gustavo", 30, 5,2));
+        jogadores.add(new Jogador("Gustavo", 30, 11,2));
+        jogadores.add(new Jogador("4", 30, 4,2));
+        jogadores.add(new Jogador("5", 30, 5,3));
+        jogadores.add(new Jogador("6", 30, 6,2));
+        jogadores.add(new Jogador("7", 30, 7,2));
+        jogadores.add(new Jogador("8", 30, 8,3));
+        jogadores.add(new Jogador("9", 30, 9,2));
+        jogadores.add(new Jogador("10", 30, 10,2));
+        jogadores.add(new Jogador("0", 30, 0,2));
         System.out.println("Carga finalizada");
 
-        ordenaLista(jogadores);
+        ordenaRanking(jogadores);
+        exibeRanking(jogadores);
     }
 
-    public static void ordenaLista(List jogadores) {
+    public static void ordenaRanking(List<Jogador> jogadores) {
         jogadores.sort(Comparator.comparingLong(Jogador::getPontuacao).reversed());
-        jogadores.forEach(System.out::println);
+    }
+
+    public static void exibeRanking(List<Jogador> jogadores) {
+        for (int i = 0; i < 10; i++) {
+            Jogador j =  jogadores.get(i);
+            System.out.println(j.getNome() + " - " + (i+1));
+        }
     }
 }
 
