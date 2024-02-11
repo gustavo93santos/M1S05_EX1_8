327e74a02609e0bb10e36951d532af6eb3f82939
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-02-11 01:01:43 -0300

    [M1S05] EX5 - Adiciona jogador a lista impedindo duplicidade.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,43 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando Jogo!");
-
         List<Jogador> jogadores = new ArrayList<>();
-        jogadores.add(new Jogador("Leonardo", 30, 10,2));
-        jogadores.add(new Jogador("Andre", 30, 15,3));
-        jogadores.add(new Jogador("Gustavo", 30, 11,2));
-        jogadores.add(new Jogador("4", 30, 4,2));
-        jogadores.add(new Jogador("5", 30, 5,3));
-        jogadores.add(new Jogador("6", 30, 6,2));
-        jogadores.add(new Jogador("7", 30, 7,2));
-        jogadores.add(new Jogador("8", 30, 8,3));
-        jogadores.add(new Jogador("9", 30, 9,2));
-        jogadores.add(new Jogador("10", 30, 10,2));
-        jogadores.add(new Jogador("0", 30, 0,2));
-        System.out.println("Carga finalizada");
+        Scanner entrada = new Scanner(System.in);
+        cargaDadosTestes(jogadores);
+        String nome;
+        int idade;
+        System.out.println("Carga inicial finalizada");
+
+        System.out.println("Bem vindo ao cadastro do jogador");
+        do {
+            System.out.print("Nome: ");
+            nome = entrada.nextLine();
+            if(existeJogador(jogadores, nome)){
+                System.out.println("Jogador com mesmo nome ja registrado, por favor insira um novo nome");
+            }else{
+                break;
+            }
+        }while (true);
+        System.out.println("Idade: ");
+        idade = Integer.parseInt(entrada.nextLine());
+        jogadores.add(new Jogador(nome,idade,0,0));
 
         ordenaRanking(jogadores);
         exibeRanking(jogadores);
+    }
+
+    public static boolean existeJogador(List<Jogador> jogadores, String nome){
+        for (Jogador jogador : jogadores) {
+            if (jogador.getNome().equalsIgnoreCase(nome)){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void ordenaRanking(List<Jogador> jogadores) {
@@ -33,6 +49,22 @@ public class Main {
             Jogador j =  jogadores.get(i);
             System.out.println(j.getNome() + " - " + (i+1));
         }
+    }
+
+
+
+    public static void cargaDadosTestes(List<Jogador> jogadores){
+        jogadores.add(new Jogador("Leonardo", 30, 10,2));
+        jogadores.add(new Jogador("Andre", 30, 15,3));
+        jogadores.add(new Jogador("Gustavo", 30, 11,2));
+        jogadores.add(new Jogador("4", 30, 4,2));
+        jogadores.add(new Jogador("5", 30, 5,3));
+        jogadores.add(new Jogador("6", 30, 6,2));
+        jogadores.add(new Jogador("7", 30, 7,2));
+        jogadores.add(new Jogador("8", 30, 8,3));
+        jogadores.add(new Jogador("9", 30, 9,2));
+        jogadores.add(new Jogador("10", 30, 10,2));
+        jogadores.add(new Jogador("0", 30, 0,2));
     }
 }
 
